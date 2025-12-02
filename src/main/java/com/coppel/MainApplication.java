7997c7fc3edbe97fd67e75e54ce232eb01e16c28
Bryan Jovanny Inzunza Jiménez <l20170704@culiacan.tecnm.mx>
2025-12-02 05:05:31 -0600

    Cambio de configuracion de CORS

--- src/main/java/com/coppel/MainApplication.java
+++ src/main/java/com/coppel/MainApplication.java
@@ -2,34 +2,11 @@ package com.coppel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MainApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MainApplication.class, args);
-    }
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**")
-                        .allowedOrigins(
-                                "http://localhost:4200", // Angular development
-                                "http://localhost:3000", // Alternative frontend port
-                                "http://localhost:8080" // Same origin
-                )
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .exposedHeaders("Content-Type", "Authorization")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
     }
 }
