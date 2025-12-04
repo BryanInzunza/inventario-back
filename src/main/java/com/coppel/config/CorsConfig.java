e627965336e001fe713f64a2d4aacaa2fbe42903
Bryan Jovanny Inzunza Jiménez <l20170704@culiacan.tecnm.mx>
2025-12-03 20:10:06 -0600

    Configuración cors

--- src/main/java/com/coppel/config/CorsConfig.java
+++ src/main/java/com/coppel/config/CorsConfig.java
@@ -15,9 +15,9 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins(
+                        "http://localhost:3000",
                         "http://localhost:4200",
-                        "http://127.0.0.1:4200",
-                        "http://localhost:8080")
+                        "http://172.19.102.148:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
                 .allowedHeaders("*")
                 .exposedHeaders("Content-Type", "Authorization", "X-Total-Count")
